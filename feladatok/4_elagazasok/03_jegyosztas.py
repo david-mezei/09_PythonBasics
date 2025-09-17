88dc24d5161c4e9adc7989db2bd5cfb013f6e2de
Dávid Mezei <mezei.david.sandor@diak.szbi-pg.hu>
2025-09-17 08:33:45 +0200

    Create 03_jegyosztas.py

---
+++ feladatok/4_elagazasok/03_jegyosztas.py
@@ -0,0 +1,23 @@
+"""
+Kérj be egy pontszámot 0-100 között, és írd ki az érdemjegyet:
+- 0-49: Elégtelen 
+- 50-64: Elégséges
+- 65-79: Közepes
+- 80-89: Jó
+- 90-100: Jeles
+"""
+
+pontszam = int(input("Adj meg egy pontszámot! [0-100] "))
+
+if pontszam <= 49:
+    print("Elégtelen")
+elif pontszam <= 64:
+    print("Elégséges")
+elif pontszam <= 79:
+    print("Közepes")
+elif pontszam <= 89:
+    print("Jó")
+elif pontszam <= 100:
+    print("Jeles")
+else:
+    print("Hibás számformátum! [0-100]")
