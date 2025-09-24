a6111b809b3b52755c8e4da8ab589e4d9b7b17e3
Mezei Dávid <mezei.david.sandor@diak.szbi-pg.hu>
2025-09-24 11:24:25 +0200

    Ciklus feladatok_Sulipy (for)

---
+++ feladatok/5_ciklusok/5_1.py
@@ -0,0 +1,12 @@
+"""
+1. feladat 
+Írj egy programot, amely kiírja a páros számokat 1 és 10 között! 
+"""
+# szam = 1
+# while szam <= 10:
+#     if szam % 2 == 0:
+#         print(szam)
+#     szam = szam + 1 
+
+for szam in range(2,11,2):
+    print(szam)
---
+++ feladatok/5_ciklusok/5_3.py
@@ -0,0 +1,7 @@
+"""
+Írj egy programot, amely kiírja a páratlan számokat csökkenő sorrendben 1 és 10 között! 
+"""
+
+for szam in range(10,0,-1):
+    if szam % 2 != 0:
+        print(szam)
